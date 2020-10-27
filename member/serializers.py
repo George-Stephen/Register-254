80ea333f704eea21b9b1c75a969c28390ec28cd0
George-Stephen <yazz777stevie@gmail.com>
2020-10-27 14:21:40 +0300

    deployment

--- member/serializers.py
+++ member/serializers.py
@@ -5,7 +5,7 @@ from .models import Member,Order
 class MemberSerializers(serializers.ModelSerializer):
     class Meta :
         model = Member
-        fields = ('id_number','full_name','date_birth','gender','phone_number','county','sub_county')
+        fields = ('id_number','full_name','gender','date_birth','phone_number','county','sub_county')
 
 
 class OrderSerializers(serializers.ModelSerializer):
